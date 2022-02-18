0af78fded5075979ba924ef3ee73c333d7542690
21P31A1232 <98534597+21P31A1232@users.noreply.github.com>
2022-02-18 09:26:10 +0530

    #include<stdio.h> #include<math.h> int main() {     int S,K;     float misunderstand;     scanf("%d%d",&S,&K);     misunderstand=S/K;     printf("%.0f", misunderstand);     return 0;
    }

---
+++ Division.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int S,K;
+    float misunderstand;
+    scanf("%d%d",&S,&K);
+    misunderstand=S/K;
+    printf("%.0f", misunderstand);
+    return 0;
+    
+}
